935599e8f3f052eabc24669ff8153d354f54c215
Vasuka <vasukaman@gmail.com>
2025-08-18 22:08:02 -0400

    Fix: Stupid query code

--- internal/sinapi/store/postgres.go
+++ internal/sinapi/store/postgres.go
@@ -94,7 +94,7 @@ func (s *Store) IncrementSinCount(apiKeyID int, description string, tags []strin
 	var sin models.Sin
 
 	err := s.db.QueryRow(context.Background(), `
-		INSERT INTO sins (api_key_id, description, count, tags, severity, emoji) VALUES ($1, $2, 1, $3, $4)
+		INSERT INTO sins (api_key_id, description, count, tags, severity, emoji) VALUES ($1, $2, 1, $3, $4, $5)
 		ON CONFLICT (api_key_id, description) DO UPDATE
 		SET count = sins.count + 1
 		RETURNING id, description, count, created_at, tags, severity, emoji`,
